bfbd3d0726b90a8ce12617b6160178a2424d0e6b
morbdeath <nahuel_barbosa1990@hotmail.com.ar>
2023-04-08 11:54:03 -0300

    Ultimos detalles

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import imagen from '../images/DH.png'
+import image from '../images/DH.png'
 
 const Footer = () => {
   return (
     <footer>
         <p>Powered by</p>
-        <img src= {imagen}/>
+        <img
+        style={{ width: '200px', marginLeft: '10px', height: '40px' }}
+        src= {image}
+        alt="DH"
+        />
     </footer>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import {ButtonTema} from '../Components/ButtonTema'
+import { Link } from 'react-router-dom'
+import {ButtonTheme} from './ButtonTheme'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
 
   return (
-    <nav className="nav">
-              <Link to="/home">Home</Link>           
-              <Link to="/favs">Favoritos </Link> 
-              <Link to="/contact">Contacto </Link> 
-              <Link to="/details">Detalles </Link> 
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
-      <ButtonTema/>
-    </nav>
+    <div>
+      <nav className="nav">
+        <Link to="/home">Home</Link>
+        <Link to="/favs">Favoritos </Link>
+        <Link to="/contact">Contacto </Link>
+        <Link to="/details">Detalles </Link>
+        {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
+
+        <ButtonTheme/>
+      </nav>
+    </div>
   )
 }
 
